0a7ce8b55a77f429b0fe895ed0f4b41cef903515
Dan Elkouby <streetwalrus@codewalr.us>
2016-11-01 00:50:03 +0200

    Revert "Build livedisplay native"
    This reverts commit 49bdb45d58356aaa3e28119f957da53c3bc40572.
    
    Breaks KCAL

--- device.mk
+++ device.mk
@@ -180,10 +180,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     lights.qcom
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
