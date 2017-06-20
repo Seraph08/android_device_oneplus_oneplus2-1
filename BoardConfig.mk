5f7ce2e57a2df2b6b4d1a62ea242bc478bda2b59
Seraph08 <davidvandenburgh@linux.com>
2017-06-19 23:53:37 -0400

    remove cmhw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,10 +110,6 @@ BOARD_USES_LIBC_WRAPPER := true
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
-
 # CNE and DPM
 TARGET_LDPRELOAD := libNimsWrap.so
 BOARD_USES_QCNE := true
