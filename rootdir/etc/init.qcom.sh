b6b4820a19dacf02badd6a2d160bb60fc439fff2
Seraph08 <d-vandenburgh@hvcc.edu>
2017-07-23 21:19:36 -0400

    squash commit merge with LineageOS

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -264,8 +264,9 @@ bootmode=`getprop ro.bootmode`
 emmc_boot=`getprop ro.boot.emmc`
 case "$emmc_boot"
     in "true")
-        if [ "$bootmode" != "charger" ]; then # start rmt_storage
+        if [ "$bootmode" != "charger" ]; then # start rmt_storage and rfs_access
             start rmt_storage
+            start rfs_access
         fi
     ;;
 esac
@@ -277,8 +278,9 @@ rm -rf /data/misc/radio/modem_config
 mkdir /data/misc/radio/modem_config
 #ifdef VENDOR_EDIT
 # Modify /data/misc/radio/modem_config authority to 770 from 660, and modify the target path to /system/etc/firmware/mbn_ota/, by hanqingpu@oneplus.cn, 20150530
+# Because we removed symlinks we have to copy from firmware partition /firmware/image/modem_pr/mcfg/configs/
 chmod 770 /data/misc/radio/modem_config
-cp -r /system/etc/firmware/mbn_ota/* /data/misc/radio/modem_config
+cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
 #endif /*VENDOR_EDIT*/
 chown -hR radio.radio /data/misc/radio/modem_config
 echo 1 > /data/misc/radio/copy_complete
