2867b46b27bc4d2405d8332ec9b5c3e6335fe448
Seraph08 <d-vandenburgh@hvcc.edu>
2017-08-09 03:32:48 -0400

    oneplus2: fix keylayouts

--- oneplus2.mk
+++ oneplus2.mk
@@ -181,7 +181,12 @@ PRODUCT_COPY_FILES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
-    $(LOCAL_PATH)/keylayout/synaptics.kl:system/usr/keylayout/synaptics.kl
+    $(LOCAL_PATH)/keylayout/synaptics.kl:system/usr/keylayout/synaptics.kl \
+    $(LOCAL_PATH)/keylayout/synaptics-rmi.kl:system/usr/keylayout/synaptics-rmi.kl
+    
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8994
 
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
