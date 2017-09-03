ac3e7bdba9881ec62b609231cd6ac847c8f8eb9f
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-09-03 11:09:58 +0200

    oneplus2: add HIDL display hal
    Change-Id: I69ed3b534abc5bb99343b05224b34d9cdd53b5c6

--- oneplus2.mk
+++ oneplus2.mk
@@ -136,7 +136,15 @@ PRODUCT_PACKAGES += \
     memtrack.qcom \
     liboverlay \
     libtinyxml
-    
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl
+
 # Data
 PRODUCT_PACKAGES += \
     librmnetctl \
