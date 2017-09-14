5e810ac4682acfa5003c0c3aaabab018a6b3761e
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    oneplus2: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- oneplus2.mk
+++ oneplus2.mk
@@ -265,6 +265,11 @@ ifneq ($(WLAN_CHIPSET),)
 PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
 endif
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # WiFi
 PRODUCT_PACKAGES += \
     hostapd \
